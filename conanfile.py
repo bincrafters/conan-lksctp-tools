08ef6adae8559c7ceb6ec57fab6419b2bfcf6203
Uilian Ries <uilianries@gmail.com>
2017-11-25 22:53:23 -0200

    Move recipe to Bincrafters
    - Updated user name to bincrafters
    - Updated build and recipe to use default template
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,8 @@
 """Conan recipe for LKSCTP Tools
 """
-from os import unlink
-from os.path import join
+import os
 from tempfile import mkdtemp
-from conans import ConanFile
-from conans import AutoToolsBuildEnvironment
-from conans.tools import download
-from conans.tools import unzip
-from conans.tools import chdir
-from conans.tools import environment_append
-from conans.tools import check_md5
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 
 
 class LKSCTPToolsConan(ConanFile):
@@ -19,50 +12,41 @@ class LKSCTPToolsConan(ConanFile):
     version = "1.0.17"
     generators = "cmake", "txt"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fpic": [True, False]}
-    default_options = "shared=True", "fpic=False"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     url = "https://github.com/uilianries/conan-lksctp-tools"
+    description = "The lksctp-tools project provides a Linux user space library for SCTP"
     author = "Uilian Ries <uilianries@gmail.com>"
     license = "GPL-2"
-    release_name = "%s-%s" % (name, version)
-    build_dir = mkdtemp(suffix=name)
+    install_dir = mkdtemp(suffix=name)
 
     def source(self):
-        tar_name = "%s.tar.gz" % self.release_name
-        download("https://github.com/sctp/lksctp-tools/archive/%s" % tar_name,
-                 tar_name)
-        check_md5(tar_name, "910a4f1d4024d71149b91f50f97eae23")
-        unzip(tar_name)
-        unlink(tar_name)
+        source_url = "https://github.com/sctp/lksctp-tools"
+        tools.get("{0}/archive/{1}-{2}.tar.gz".format(source_url, self.name, self.version))
+        extracted_dir = self.name + "-" + self.name + "-" + self.version
+        os.rename(extracted_dir, "sources")
+
+    def configure(self):
+        del self.settings.compiler.libcxx
 
     def build(self):
         env_build = AutoToolsBuildEnvironment(self)
-        env_build.fpic = self.options.fpic
-        with environment_append(env_build.vars):
-            with chdir("%s-%s" % (self.name, self.release_name)):
+        env_build.fpic = True
+        with tools.environment_append(env_build.vars):
+            with tools.chdir("sources"):
                 self.run("./bootstrap")
                 library_type = "--disable-static" if self.options.shared else "--disable-shared"
-                self.run("./configure --prefix=%s --disable-tests %s" %
-                         (self.build_dir, library_type))
+                self.run("./configure --prefix=%s --disable-tests %s" % (self.install_dir, library_type))
                 self.run("make")
                 self.run("make install")
 
     def package(self):
-        self.copy(
-            pattern="*.h", dst="include", src=join(self.build_dir, "include"))
-        self.copy(
-            pattern="*.a",
-            dst="lib",
-            src=join(self.build_dir, "lib"),
-            keep_path=False)
-        self.copy(
-            pattern="*.so*",
-            dst="lib",
-            src=join(self.build_dir, "lib"),
-            keep_path=False)
-        self.copy(pattern="*", dst="bin", src=join(self.build_dir, "bin"))
+        self.copy(pattern="COPYING", dst=".", src="sources")
+        self.copy(pattern="*.h", dst="include", src=os.path.join(self.install_dir, "include"))
+        self.copy(pattern="*.a", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ['sctp', 'withsctp']
+        self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("dl")
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,34 +1,19 @@
-"""Recipe validation for lksctp-tools-1.0.17
-"""
-from os import getenv
-from conans import ConanFile, CMake
+import os
+from conans import ConanFile, CMake, tools, RunEnvironment
 
 
-class TestLKSCTPToolsConan(ConanFile):
-    """Build test using target package and execute all tests
-    """
-    target = "lksctp-tools"
-    name = "%s-test" % target
-    version = "1.0.17"
-    author = "Uilian Ries <uilianries@gmail.com>"
-    license = "GPL-2"
+class LksctpTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    channel = getenv("CONAN_CHANNEL", "testing")
-    user = getenv("CONAN_USERNAME", "uilianries")
-    requires = "%s/%s@%s/%s" % (target, version, user, channel)
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        cmake.configure(
-            self, source_dir=self.conanfile_directory, build_dir="./")
-        cmake.build(self)
-
-    def imports(self):
-        self.copy(pattern="*.so*", dst="bin", src="lib")
+        cmake = CMake(self)
+        cmake.configure(source_dir=self.conanfile_directory, build_dir=os.getcwd())
+        cmake.build()
 
     def test(self):
-        cmake = CMake(self.settings)
-        cmake.configure(
-            self, source_dir=self.conanfile_directory, build_dir="./")
-        cmake.build(self, target="test")
+        with tools.environment_append(RunEnvironment(self).vars):
+            if self.settings.os == "Windows":
+                self.run(os.path.join("bin","test_package"))
+            else:
+                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ['DYLD_LIBRARY_PATH'],os.path.join("bin","test_package")))
