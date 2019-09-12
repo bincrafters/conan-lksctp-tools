0133bf7ec44c3f31c8c98672b0e3aa78daafd7ab
bincrafters-user <bincrafters@gmail.com>
2019-09-12 01:51:58 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.10.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/bincrafters/conan-lksctp-tools.svg?branch=release/1.0.17)](https://travis-ci.org/bincrafters/conan-lksctp-tools)
+[![Build Status](https://travis-ci.com/bincrafters/conan-lksctp-tools.svg?branch=release/1.0.17)](https://travis-ci.com/bincrafters/conan-lksctp-tools)
 [![CircleCI](https://circleci.com/gh/bincrafters/conan-lksctp-tools/tree/stable%2F1.0.17.svg?style=svg)](https://circleci.com/gh/bincrafters/conan-lksctp-tools/tree/stable%2F1.0.17)
 [![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)
 
--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from conans.errors import ConanInvalidConfiguration
 import os, glob, shutil
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 
@@ -11,31 +12,31 @@ class LKSCTPToolsConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False], "with_sctp": [True, False]}
-    default_options = ("shared=False", "fPIC=True", "with_sctp=False")
+    default_options = {'shared': False, 'fPIC': True, 'with_sctp': False}
     homepage = "http://lksctp.sourceforge.net/"
     url = "https://github.com/bincrafters/conan-lksctp-tools"
     description = "The lksctp-tools project provides a Linux user space library for SCTP"
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "GPL-2.0, LGPL-2.1"
     exports = ["LICENSE.md"]
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
 
     def source(self):
         source_url = "https://github.com/sctp/lksctp-tools"
         tools.get("{0}/archive/{1}-{2}.tar.gz".format(source_url, self.name, self.version))
         extracted_dir = self.name + "-" + self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
     def configure(self):
         if self.settings.os != "Linux":
-            raise Exception("Linux Kernel SCTP Tools is only supported for Linux.")
+            raise ConanInvalidConfiguration("Linux Kernel SCTP Tools is only supported for Linux.")
         del self.settings.compiler.libcxx
 
     def build(self):
         env_build = AutoToolsBuildEnvironment(self)
         env_build.fpic = self.options.fPIC
         with tools.environment_append(env_build.vars):
-            with tools.chdir(self.source_subfolder):
+            with tools.chdir(self._source_subfolder):
                 self.run("./bootstrap")
 
             config_args = []
@@ -43,12 +44,12 @@ class LKSCTPToolsConan(ConanFile):
             config_args.append("--prefix=%s" % self.package_folder)
             config_args.append("--disable-tests")
 
-            env_build.configure(configure_dir=self.source_subfolder, args=config_args)
+            env_build.configure(configure_dir=self._source_subfolder, args=config_args)
             env_build.make()
             env_build.make(args=["install"])
 
     def package(self):
-        self.copy(pattern="COPYING*", dst="licenses", src=self.source_subfolder, ignore_case=True, keep_path=False)
+        self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder, ignore_case=True, keep_path=False)
         self.move_withsctp()
 
     def package_info(self):
